f178ed0bd553350d6bd0970c39581aa9c63370b8
Bruno Mistro <34082572+brunomistro@users.noreply.github.com>
2021-12-03 13:24:06 -0300

    aula 3

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,11 +1,19 @@
 export class Negociacao {
-	private _data;
-	private _quantitdade;
-	private _valor;
+	private _data: Date;
+	private _quantitdade: number;
+	private _valor: number;
 
-	constructor(data, quantitdade, valor) {
+	constructor(data: Date, quantitdade: number, valor: number) {
 		this._data = data;
 		this._quantitdade = quantitdade;
 		this._valor = valor;
 	}
+
+	get data(): Date { return this._data }
+
+	get quantidade(): number { return this._quantitdade }
+
+	get valor(): number { return this._valor }
+
+	get volume(): number { return this._quantitdade * this._valor }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -4,4 +4,8 @@ export class Negociacao {
         this._quantitdade = quantitdade;
         this._valor = valor;
     }
+    get data() { return this._data; }
+    get quantidade() { return this._quantitdade; }
+    get valor() { return this._valor; }
+    get volume() { return this._quantitdade * this._valor; }
 }
